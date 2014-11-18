f8de774cdbce8d8bc4108cf872213cafec6aede1
Vladislav Lipskiy <vladislav.lipskiy@gmail.com>
2014-11-18 15:42:16 +0200

    Fix for Android NDK r10c.

--- coffeecatch.c
+++ coffeecatch.c
@@ -43,10 +43,7 @@
 #include <assert.h>
 #include <signal.h>
 #include <setjmp.h>
-#if defined(__ANDROID__) && !defined(__BIONIC_HAVE_UCONTEXT_T) && \
-    defined(__arm__) && !defined(__BIONIC_HAVE_STRUCT_SIGCONTEXT)
-#include <asm/sigcontext.h>
-#endif 
+#include <sys/ucontext.h>
 #if (defined(USE_UNWIND) && !defined(USE_CORKSCREW))
 #include <unwind.h>
 #endif
@@ -88,130 +85,6 @@ static const int native_sig_catch[SIG_CATCH_COUNT + 1]
 /* Maximum value of a caught signal. */
 #define SIG_NUMBER_MAX 32
 
-#if defined(__ANDROID__)
-#ifndef ucontext_h_seen
-#define ucontext_h_seen
-
-/* stack_t definition */
-#include <asm/signal.h>
-
-#if defined(__arm__)
-
-/* Taken from richard.quirk's header file. (Android does not have it) */
-
-#if !defined(__BIONIC_HAVE_UCONTEXT_T)
-typedef struct ucontext {
-  unsigned long uc_flags;
-  struct ucontext *uc_link;
-  stack_t uc_stack;
-  struct sigcontext uc_mcontext;
-  unsigned long uc_sigmask;
-} ucontext_t;
-#endif
-
-#elif defined(__i386__)
-
-/* Taken from Google Breakpad. */
-
-/* 80-bit floating-point register */
-struct _libc_fpreg {
-  unsigned short significand[4];
-  unsigned short exponent;
-};
-
-/* Simple floating-point state, see FNSTENV instruction */
-struct _libc_fpstate {
-  unsigned long cw;
-  unsigned long sw;
-  unsigned long tag;
-  unsigned long ipoff;
-  unsigned long cssel;
-  unsigned long dataoff;
-  unsigned long datasel;
-  struct _libc_fpreg _st[8];
-  unsigned long status;
-};
-
-typedef uint32_t  greg_t;
-
-typedef struct {
-  uint32_t gregs[19];
-  struct _libc_fpstate* fpregs;
-  uint32_t oldmask;
-  uint32_t cr2;
-} mcontext_t;
-
-enum {
-  REG_GS = 0,
-  REG_FS,
-  REG_ES,
-  REG_DS,
-  REG_EDI,
-  REG_ESI,
-  REG_EBP,
-  REG_ESP,
-  REG_EBX,
-  REG_EDX,
-  REG_ECX,
-  REG_EAX,
-  REG_TRAPNO,
-  REG_ERR,
-  REG_EIP,
-  REG_CS,
-  REG_EFL,
-  REG_UESP,
-  REG_SS,
-};
-
-#if !defined(__BIONIC_HAVE_UCONTEXT_T)
-typedef struct ucontext {
-  uint32_t uc_flags;
-  struct ucontext* uc_link;
-  stack_t uc_stack;
-  mcontext_t uc_mcontext;
-} ucontext_t;
-#endif
-
-#elif defined(__mips__)
-
-/* Taken from Google Breakpad. */
-
-typedef struct {
-  uint32_t regmask;
-  uint32_t status;
-  uint64_t pc;
-  uint64_t gregs[32];
-  uint64_t fpregs[32];
-  uint32_t acx;
-  uint32_t fpc_csr;
-  uint32_t fpc_eir;
-  uint32_t used_math;
-  uint32_t dsp;
-  uint64_t mdhi;
-  uint64_t mdlo;
-  uint32_t hi1;
-  uint32_t lo1;
-  uint32_t hi2;
-  uint32_t lo2;
-  uint32_t hi3;
-  uint32_t lo3;
-} mcontext_t;
-
-#if !defined(__BIONIC_HAVE_UCONTEXT_T)
-typedef struct ucontext {
-  uint32_t uc_flags;
-  struct ucontext* uc_link;
-  stack_t uc_stack;
-  mcontext_t uc_mcontext;
-} ucontext_t;
-#endif
-
-#else
-#error "Architecture is not supported (unknown ucontext layout)"
-#endif
-
-#endif
-
 #ifdef USE_CORKSCREW
 typedef struct map_info_t map_info_t;
 /* Extracted from Android's include/corkscrew/backtrace.h */
@@ -238,8 +111,6 @@ typedef void (*t_get_backtrace_symbols)(const backtrace_frame_t* backtrace,
                                         backtrace_symbol_t* symbols);
 typedef void (*t_free_backtrace_symbols)(backtrace_symbol_t* symbols,
                                          size_t frames);
-#endif
-
 #endif
 
 /* Process-wide crash handler structure. */
